54da3a5676a7c21c54eb54da79f11bd230d86ac7
hanaNrvtn <nrvtnhana@gmail.com>
2023-05-22 11:56:17 -0700

    Discountable interface added and implemented by DigitalEquipment.java, Pencil.java, Pen.java.

--- proj01/src/model/product/DigitalEquipment.java
+++ proj01/src/model/product/DigitalEquipment.java
@@ -1,8 +1,10 @@
 package model.product;
 
-abstract public class DigitalEquipment extends Product {
+abstract public class DigitalEquipment extends Product implements Comparable<Product>, Discountable{
     private double weight;
     private String dimension;
+    private double discountPercentage;
+    private int discountCapacity;
 
     public DigitalEquipment(double weight, String dimension, String name, int price, int numberOfProduct) {
         super(ProductCategory.DIGITAL_EQUIPMENT, name, price, numberOfProduct);
@@ -24,6 +26,46 @@ abstract public class DigitalEquipment extends Product {
 
     public void setDimension(String dimension) {
         this.dimension = dimension;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public int getDiscountCapacity() {
+        return discountCapacity;
+    }
+
+    public void setDiscountCapacity(int discountCapacity) {
+        this.discountCapacity = discountCapacity;
+    }
+
+    @Override
+    public int compareTo(Product product){
+        DigitalEquipment digitalEquipment=(DigitalEquipment) product;
+        if((digitalEquipment instanceof DataStorageEquipment) && !(this instanceof DataStorageEquipment))
+            return 1;
+        else if(!(digitalEquipment instanceof DataStorageEquipment) && (this instanceof DataStorageEquipment))
+            return -1;
+        else if (digitalEquipment instanceof DataStorageEquipment)
+            return this.compareTo(digitalEquipment);
+        else return 0;
+    }
+
+    @Override
+    public void applyDiscount(double percentage, int capacity){
+        this.discountPercentage=percentage;
+        this.discountCapacity=capacity;
+    }
+
+    @Override
+    public void rescindDiscount(){
+        this.discountPercentage=0;
+        this.discountCapacity=0;
     }
 
         @Override
--- proj01/src/model/product/Pen.java
+++ proj01/src/model/product/Pen.java
@@ -1,7 +1,9 @@
 package model.product;
 
-public class Pen extends Stationary {
+public class Pen extends Stationary implements Discountable{
     private PenColor color;
+    private double discountPercentage;
+    private int discountCapacity;
 
     public Pen(String color, String manufacturingCountry, String name, int price, int numberOfNewProduct) {
         super(manufacturingCountry, name, price, numberOfNewProduct);
@@ -15,6 +17,35 @@ public class Pen extends Stationary {
     public void setColor(PenColor color) {
         this.color = color;
     }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public int getDiscountCapacity() {
+        return discountCapacity;
+    }
+
+    public void setDiscountCapacity(int discountCapacity) {
+        this.discountCapacity = discountCapacity;
+    }
+
+    @Override
+    public void applyDiscount(double percentage, int capacity){
+        this.discountPercentage=percentage;
+        this.discountCapacity=capacity;
+    }
+
+    @Override
+    public void rescindDiscount(){
+        this.discountPercentage=0;
+        this.discountCapacity=0;
+    }
+
 
     @Override
     public String toString() {
