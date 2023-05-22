83d3765bfe720a0167329b75c9277e4d3a2d8abd
hanaNrvtn <nrvtnhana@gmail.com>
2023-05-22 13:05:43 -0700

    DiscountCode.java class and DiscountType.java enum added. Customer.java edited: arrayList of discount codes added. Invoice.java edited: arrayList of applied discount codes and totalDiscount and finalAmount fields added.

---
+++ proj01/src/model/product/DiscountType.java
@@ -0,0 +1,5 @@
+package model.product;
+
+public enum DiscountType {
+    LOYALTY, WELCOMING, ENCOURAGING
+}
--- proj01/src/model/user/Invoice.java
+++ proj01/src/model/user/Invoice.java
@@ -1,40 +1,66 @@
 package model.user;
 
-import model.product.Product;
+import model.product.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Invoice {
     static int counter;
     private final String ID;
-    private final String date;
-    private final int totalAmount;
+    private final LocalDate date;
+    private final double totalAmount;
+    private final double totalDiscount;
+    private final double finalAmount;
     private final ArrayList<Product> boughtProducts;
+    private final ArrayList<DiscountCode> appliedDiscountCodes;
 
-    public Invoice(String date, ArrayList<Product> boughtProducts) {
+    public Invoice(LocalDate date, ArrayList<Product> boughtProducts) {
         ++counter;
         this.date = date;
         this.boughtProducts = boughtProducts;
+        this.appliedDiscountCodes=new ArrayList<>();
         this.totalAmount = calculateTotalAmount();
+        this.totalDiscount=calculateTotalDiscount();
+        this.finalAmount=calculateFinalAmount();
         ID = IDMaker();
     }
 
     private String IDMaker() {
-        return counter + date.split("-")[2] + date.split("-")[2];
+        return counter + date.toString().split("-")[2] + date.toString().split("-")[2];
     }
 
-    private int calculateTotalAmount() {
-        int totalAmount = 0;
-        for (Product a : boughtProducts)
-            totalAmount += a.getPrice();
+    private double calculateTotalAmount() {
+        double totalAmount = 0;
+        for (Product a : boughtProducts){
+            if(a instanceof DigitalEquipment)
+                totalAmount+=a.getPrice()*((DigitalEquipment) a).getDiscountPercentage()*0.01;
+            else if(a instanceof Pen)
+                totalAmount+=a.getPrice()*((Pen) a).getDiscountPercentage()*0.01;
+            else if(a instanceof Pencil)
+                totalAmount+=a.getPrice()*((Pencil) a).getDiscountPercentage()*0.01;
+            else totalAmount += a.getPrice();
+        }
         return totalAmount;
+    }
+
+    private double calculateTotalDiscount(){
+        if(appliedDiscountCodes.size()==0) return 0;
+        double totalDiscount=1;
+        for(DiscountCode a: appliedDiscountCodes)
+            totalDiscount*=a.getPercentage()*0.01;
+        return totalDiscount;
+    }
+
+    private double calculateFinalAmount(){
+        return this.totalAmount-(this.totalAmount*this.totalDiscount);
     }
 
     public String getID() {
         return ID;
     }
 
-    public String getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
@@ -42,8 +68,20 @@ public class Invoice {
         return totalAmount;
     }
 
+    public double getTotalDiscount() {
+        return totalDiscount;
+    }
+
+    public double getFinalAmount() {
+        return finalAmount;
+    }
+
     public ArrayList<Product> getBoughtProducts() {
         return boughtProducts;
+    }
+
+    public ArrayList<DiscountCode> getAppliedDiscountCodes() {
+        return appliedDiscountCodes;
     }
 
     @Override
