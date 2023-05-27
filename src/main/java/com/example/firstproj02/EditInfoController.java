b27406f53c45c1ff4d3cc54cbf6814a3fcaefe29
hanaNrvtn <nrvtnhana@gmail.com>
2023-05-27 06:04:13 -0700

    edit-info-view.fxml and EditInfoController.java added: no Application class due to multiple fxml loading in one scene (customer panel).

---
+++ src/main/java/com/example/firstproj02/EditInfoController.java
@@ -0,0 +1,169 @@
+package com.example.firstproj02;
+
+import com.example.firstproj02.controller.CustomerController;
+import com.example.firstproj02.model.accounts.Customer;
+import com.example.firstproj02.model.exceptions.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditInfoController implements Initializable {  // set current info as promot text
+    private Customer customer;
+    CustomerController customerController;
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private Label firstNameStatusLabel;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private Label lastNameStatusLabel;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private Label emailStatusLabel;
+
+    @FXML
+    private TextField phoneNumberTextField;
+
+    @FXML
+    private Label phoneNumberStatusLabel;
+
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    private Label userNameStatusLabel;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Label passwordStatusLabel;
+
+    @FXML
+    private Button submitButton;
+
+    boolean  emailIsValid, phoneNumberIsValid, passwordIsValid;
+
+    @FXML
+    void firstNameTextField(KeyEvent event) {
+    }
+
+    @FXML
+    void lastNameTextField(KeyEvent event) {
+    }
+
+    @FXML
+    void emailTextField(KeyEvent event) {
+    }
+
+    @FXML
+    void phoneNumberTextField(KeyEvent event) {
+
+    }
+
+    @FXML
+    void userNameTextField(KeyEvent event) {
+    }
+
+    @FXML
+    void passwordTextField(KeyEvent event){
+   }
+
+    @FXML
+    void submitButton(ActionEvent event) {
+        System.out.println(emailIsValid);
+        System.out.println(phoneNumberIsValid);
+        System.out.println(passwordIsValid);
+
+        if((emailIsValid || emailTextField.getText().equals("")) && (phoneNumberIsValid || phoneNumberTextField.getText().equals("")) && (passwordIsValid || passwordTextField.getText().equals(""))){
+            System.out.println("ready to edit");
+            /*
+            firstNameStatusLabel.setText("ready to edit");
+            if(!firstNameTextField.getText().equals("")) customerController.editFirstName(customer, firstNameTextField.getText());
+            if(!firstNameTextField.getText().equals("")) customerController.editLastName(customer, lastNameTextField.getText());
+            if(!emailTextField.getText().equals("")) customerController.editEmail(customer, emailTextField.getText());
+            if(!phoneNumberTextField.getText().equals("")) customerController.editPhoneNumber(customer, phoneNumberTextField.getText());
+            if(!passwordTextField.getText().equals("")) customerController.editPassword(customer, passwordTextField.getText());
+             */
+        }
+
+
+    }
+
+    private void setPromptTests(){     // maybe setText()
+        firstNameTextField.setPromptText(customer.getFirstName());
+        lastNameTextField.setPromptText(customer.getLastName());
+        emailTextField.setPromptText(customer.getEmail());
+        phoneNumberTextField.setPromptText(customer.getPhoneNumber());
+        userNameTextField.setPromptText(customer.getUserName());
+        passwordTextField.setPromptText(customer.getPassword());
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        customerController=CustomerController.getInstance();
+        //setPromptTests();
+
+        emailTextField.textProperty().addListener((p, o, n)-> {   // send them to methods
+            try {
+                customerController.checkEmailRegex(emailTextField.getText());
+                customerController.checkEmailAvailability(emailTextField.getText());
+                emailIsValid=true;
+                emailStatusLabel.setText("valid email");
+            } catch (InvalidEmailException e) {
+                emailIsValid=false;
+                emailStatusLabel.setText("invalid email");
+            } catch (UnavailableEmailException e) {
+                emailIsValid=false;
+                emailStatusLabel.setText("unavailable email");
+            }
+        });
+
+
+        phoneNumberTextField.textProperty().addListener((p, o, n)-> {
+            try {
+                customerController.checkPhoneNumberRegex(phoneNumberTextField.getText());
+                customerController.checkPhoneNumberAvailability(phoneNumberTextField.getText());
+                phoneNumberIsValid=true;
+                phoneNumberStatusLabel.setText("valid phone number");
+            } catch (InvalidPhoneNumberException e) {
+                phoneNumberIsValid=false;
+                phoneNumberStatusLabel.setText("invalid phone number");
+            } catch (UnavailablePhoneNumberException e) {
+                phoneNumberIsValid=false;
+                phoneNumberStatusLabel.setText("unavailable phone number");
+            }
+        });
+
+        passwordTextField.textProperty().addListener((p, o, n)-> {
+            try {
+                customerController.checkPasswordRegex(passwordTextField.getText());
+                passwordStatusLabel.setText("strong password");
+                passwordIsValid=true;
+            } catch (InvalidPasswordException e) {
+                passwordIsValid=false;
+                passwordStatusLabel.setText("not strong password");
+            }
+        });
+    }
+}
